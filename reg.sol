5983be35122c1091bd5c227bc6b5f3ffe3ad632a
18dew <harsh.patel54@gmail.com>
2016-04-02 10:46:32 +0530

    reg.sol add

---
+++ reg.sol
@@ -0,0 +1,18 @@
+contract registry { 
+    address public admin; 
+    mapping (address => bool) shareholder;
+    mapping (uint => address) list;
+    bool public state = false; 
+    uint no =0;
+    function init( ) {
+        if (state == false){    state = true;     admin = msg.sender;
+        }else { throw;} 
+    } 
+    function register ( address ne) {    if (admin == msg.sender) {     shareholder[ne] = true;   no = no+1; list[no] =ne; }    else { throw; }    }
+    function check( address chk) returns (bool val){    val = shareholder[chk];     }
+    function remove (address rmv)  { if (admin == msg.sender) { shareholder[rmv] = false; }  else { throw; } }
+    function check_no( uint chk) returns (address addr){    
+        if (admin == msg.sender) {    addr = list[chk];    }
+	else { throw;  }    }
+}
+
