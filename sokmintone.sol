cabc76fb6ab79bd8e9a3beef640a00b0c1ee02e9
Sirus Knight <sirus.knight@socialkapital.co>
2016-06-12 18:40:46 +0100

    Update sokmintone.sol

--- sokmintone.sol
+++ sokmintone.sol
@@ -1,308 +1,201 @@
-
-SECURITY WARNING
-All internal functions have been set to public to testing purposes. Voting default values have not been set.
-
-/////////////////////////////////////////////////////////////////////
-contract MintAuthVote_1 {
-//////////////////////////////////COMMITTEE KEYS/////////////////////////////
+contract SokMintOne {
+/////////////////////////COMMITTEE KEYS///////////////////////////////////
+address Zeus = 0x38f388fadf4a6a35c61c3f88194ec5ae162c8944;
 address Apollo = 0x06400992be45bc64a52b5c55d3df84596d6cb4a1;
+address Atlas = 0xb0dcdc575ef06dc30aaea069d8043c9d463c931c;
+///////////////////////////////DATA STRUCTURES/////////////////////////
 
-/*contract whitelist data structure*/
-struct MinAuthVote_1 {
-bool authorised;
-address addr;  
-}
-/*contract whitelist */
-mapping (uint8 => MinAuthVote_1) public MinAuthVotePass;
-    
-/* store of mandatory proposal details*/	
-struct Proposal {
-string ProposalTitle;
-string DocUrl;
-string DocHash;
-uint32 NewtonValue;
-uint256 Duration;       //proposal duration in blocks
-uint256 StartingBlock; 
-uint256 EndingBlock;
-uint256 IssuedFlorin; //number of issued florin
-uint8 Yes;       //interger for yes vote
-uint8 No;       //interger for no vote
-uint8 Abstain; //interger for abstain
-uint8 ChangeVoteRules;    //change voting rules, if vote passes
-uint8 AuthorityProposal; //change authority given to zeus key
-}	
-/*contracts core parameters*/
-struct CorePara {
-uint8 ShareQuorum;
-uint8 YesShares;	
-uint8 YesVoters;
-bool ActivePro;
-bool ActiveAuthPro;
-bool ActiveRulesPro;
-uint256 ConfAuth;	
-}	
-/*store parameters for changing voting calculation figures*/	
-struct ChangeVoteCalculator{
-uint8 ChangeShareQuorum;
-uint8 ChangeYesShares;	
-uint8 ChangeYesVoters;
-}
-/*store parameters for changing key authority*/
-struct ChangeAuthority {
-uint8 Key;
-uint8 Auth;
-}
-/*store of voting result details*/	
-struct VotingResult {
-uint256 NumYesVotes;
-uint256 NumNoVotes;
-uint256 NumAbstainVotes;
-uint256 NumVoters;
-uint256 AmountYesShares;
-uint256 TotalVoteShares;
-string VotePassed;		
-}					
-/*initialise structs*/	
-Proposal Prop;
-CorePara Core;
-ChangeVoteCalculator ChangeVoteCalc;
-VotingResult VoteResult;
-ChangeAuthority ChangeA;
-
-/*proposal tracker*/
-uint256 ProposalID = 1;
-
-/*proposal logging event*/
-event ProposalLog (uint256 indexed ProposalID, string ProposalTitle, string DocUrl, 
-string DocHash, uint32 NewtonValue, uint256 StartingBlock, uint256 EndingBlock, 
-uint8 ChangeVoteRules, uint8 AuthorityProposal);
- 
-/*voting result logging event*/
-event VotingResultLog (uint256 indexed ProposalID, string ProposalTitle, string DocUrl, 
-uint256 NumVoters, uint256 NumYesVotes, uint256 NumNoVotes, uint256 NumAbstainVotes, 
-uint256 AmountYesShares, uint256 TotalVoteShares, string VotePassed);
-
-/*constructor function*/	
-function MintAuthVote_1 (){
-Core.ShareQuorum = 60;	
-Core.YesShares = 70;
-Core.YesVoters = 70;
-Core.ActivePro = false;
-Core.ActiveAuthPro = false;
-Core.ActiveRulesPro = false;
-Core. ConfAuth = block.number + 5000;		
-}	
-/////////////////////////INITIALIZATION PROTOCOL////////////////////////
-function InitMintAuth(uint8 accountid_mav1,address addr_mav1){
-
-if(msg.sender != Apollo) throw; //check who is calling the function  
-if(block.number > Core.ConfAuth) throw; //check if the initialization window has closed     
-
-MinAuthVotePass[accountid_mav1].authorised = true;
-MinAuthVotePass[accountid_mav1].addr = addr_mav1;
-}/////////////////////////////////////////////////////////////
-
-/*function for hard reset voting process*/
-function Reset() {
-if(msg.sender != Apollo) throw; //check who is calling the function
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-if((SokMintOneCall.SirBal(msg.sender)) < 1000) throw;
-delete Prop;
-delete ChangeVoteCalc;
-delete VoteResult;
-delete ChangeA;
-ProposalID++;	
-Core.ActivePro = false;
-Core.ActiveAuthPro = false;
-Core.ActiveRulesPro = false;
-}//////////////////////////////////////////////////////
-
-/*function to derive this contract's address*/
-function GetContractAddr() constant returns (address){
-return this;}	
-address ContractAddr = GetContractAddr();
-
-/*function for inputting mandatory proposal details*/	
-function NewMintAuthProp(
-string ProposalTitle,
-string DocUrl,
-string DocHash,
-uint32 NewtonValue,
-uint256 Duration,
-uint8 Yes,
-uint8 No,
-uint8 Abstain,
-uint8 ChangeVoteRules,
-uint8 AuthorityProposal) returns (bool sucess){
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-if((SokMintOneCall.SirBal(msg.sender)) < 2) throw;
-if (NewtonValue > 2) throw;
-if (Duration > 16000) throw;
-if (Core.ActivePro == true) throw;
-if (Core.ActiveRulesPro == false && ChangeVoteRules ==1) throw;
-if (Core.ActiveAuthPro == false && AuthorityProposal ==1) throw;
-Prop.ProposalTitle = ProposalTitle;
-Prop.DocUrl = DocUrl;
-Prop.DocHash = DocHash;
-Prop.NewtonValue = NewtonValue;
-Prop.Duration = Duration;
-Prop.Yes = Yes;
-Prop.No = No;
-Prop.Abstain = Abstain;
-Prop.StartingBlock = block.number;
-Prop.EndingBlock = block.number + Duration;
-/*set up call to sok treasury one*/
-SokTreasuryOne
-SokTreasuryOneCall = SokTreasuryOne(MinAuthVotePass[8].addr);
-Prop.IssuedFlorin = (SokTreasuryOneCall.IssuedShares());
-Prop.ChangeVoteRules = ChangeVoteRules;	
-Prop.AuthorityProposal = AuthorityProposal;	
-Core.ActivePro = true;	
-/*proposal event*/
-ProposalLog (ProposalID, ProposalTitle,DocUrl,DocHash,NewtonValue,
-Prop.StartingBlock,Prop.EndingBlock, ChangeVoteRules, AuthorityProposal); 
-return true;	
-}///////////////////////////////////////////////////
+uint8 FreezeFlorin;
+uint256 ConfMint;
 	
-/*function for inputting details for changing voting rules*/
-function NewRulesProposal(
-uint8 ChangeShareQuorum,
-uint8 ChangeYesShares,	
-uint8 ChangeYesVoters) returns (bool sucess){
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-if((SokMintOneCall.SirBal(msg.sender)) < 1000) throw;  
-if(ChangeShareQuorum < 40 || ChangeShareQuorum > 90) throw;
-if(ChangeYesShares < 40 || ChangeYesShares > 90) throw;
-if(ChangeYesVoters < 40 || ChangeYesVoters > 90) throw;
-if(Core.ActivePro == true) throw;
-ChangeVoteCalc.ChangeShareQuorum = ChangeShareQuorum;
-ChangeVoteCalc.ChangeYesShares = ChangeYesShares;
-ChangeVoteCalc.ChangeYesVoters = ChangeYesVoters;
-Core.ActiveRulesPro = true; 
-return true;
+struct MintOneCoins{
+uint256 Florin;
+uint256 Newton;
+uint256 Sirus;   
 }///////////////////////////////////////////
-
-
-/*function for inputting details for changing key authority*/
-function NewAuthProposal(uint8 key_mav1, uint8 auth_mav1)	
-returns (bool sucess){	
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-if((SokMintOneCall.SirBal(msg.sender)) < 2) throw;  
-if(Core.ActivePro == true) throw;
-ChangeA.Key = key_mav1;
-ChangeA.Auth = auth_mav1;
-Core.ActiveAuthPro = true;
-return true;
+struct MintOneList{
+bool authorised; 
+address addr;  
 }///////////////////////////////////////////
+struct NewtonList{
+bool move; 
+}///////////////////////////////////////////
+struct SirusList{
+bool movement;	
+}	
+/////////////////////////////////DATA STORES//////////////////////////		
+/*balances of sok primary coins*/
+mapping (address => MintOneCoins) public balanceOf;
+/*contract whitelist*/
+mapping (uint8 => MintOneList) public MintOnePass;
+/*newton whitelist*/
+mapping (address => NewtonList) public NewtonPass; 
+/*sirus whitelist*/	
+mapping (address => SirusList) public SirusPass;	
 
-/*shareholders vote on proposals*/
-function BallotBox (uint8 Vote) returns (uint256 NewtonLog){
-address BallotCaller = msg.sender;	
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-if((SokMintOneCall.FreezeInfo()) == 0) throw;
-/*set up call to sok vote register*/
-SokVoteRegister
-SokVoteRegisterCall = SokVoteRegister(MinAuthVotePass[10].addr);
-if((SokVoteRegisterCall.Read(ProposalID, BallotCaller))== true) throw;
-if(Vote != Prop.Yes && Vote != Prop.No && Vote != Prop.Abstain) throw;
-if(block.number > Prop.EndingBlock) throw;
-/*call sok mint one*/
-uint256 VoterFloBalance = (SokMintOneCall.FloBal(BallotCaller));
-if (VoterFloBalance < 100) throw;
-uint256 VoterReward = Prop.NewtonValue * VoterFloBalance;
-/*set up call to sok treasury one*/
-SokTreasuryOne
-SokTreasuryOneCall = SokTreasuryOne(MinAuthVotePass[8].addr);
-(SokTreasuryOneCall.POV(BallotCaller, VoterReward));
-if(Vote == Prop.Yes){
-VoteResult.NumYesVotes +=1;
-VoteResult.AmountYesShares += VoterFloBalance;
-VoteResult.TotalVoteShares += VoterFloBalance;
+////////////////////////////////////TRANSACTION LOGS//////////////////////////////		
+/*broacast coin transactions on the blockchain*/
+event Transfer(address indexed from, address indexed to, uint256 value);
+
+////////////////////////////////COIN MINT////////////////////////////
+/*Initializes contract with initial supply tokens to the creator of the contract */
+function SokMintOne() {
+balanceOf[Zeus].Florin = 5000000000000;
+balanceOf[Zeus].Newton = 900000000000000;
+balanceOf[Zeus].Sirus = 100000000;
+FreezeFlorin = 0;
+ConfMint = block.number + 13000;	
+}/////////////////////////////////////////////////////////
+
+///////////////////////INITIALIZATION PROTOCOL////////////////////////
+function InitMintOne( uint8 accountid_sm1,address addr_sm1){
+/*check if the initialization window has closed*/
+if(block.number > ConfMint) throw; 
+if(msg.sender == Zeus){   
+MintOnePass[accountid_sm1].authorised = true;
+MintOnePass[accountid_sm1].addr = addr_sm1;
 }
-if(Vote == Prop.No){
-VoteResult.NumNoVotes +=1;
-VoteResult.TotalVoteShares += VoterFloBalance;
+if(msg.sender == Apollo){
+MintOnePass[accountid_sm1].authorised = true;
+MintOnePass[accountid_sm1].addr = addr_sm1;
 }
-if(Vote == Prop.Abstain){
-VoteResult.NumAbstainVotes +=1;
-VoteResult.TotalVoteShares += VoterFloBalance;
+}////////////////////////////////////////////////////////////////////////
+
+/////////////////////CREATE ACCESS KEYS////////////////////////
+function CreateKey( uint8 accountid_sm12,address addr_sm12){ 
+if(msg.sender == Apollo){
+if(MintOnePass[2].authorised != true) throw;     
+MintOnePass[accountid_sm12].authorised = true;
+MintOnePass[accountid_sm12].addr = addr_sm12;
 }
-VoteResult.NumVoters +=1;
-(SokVoteRegisterCall.Write(ProposalID, BallotCaller));
-return VoterReward;
-}/////////////////////////end of ballotbox function
+if(msg.sender == Atlas){
+if(MintOnePass[4].authorised != true) throw;
+MintOnePass[accountid_sm12].authorised = true;
+MintOnePass[accountid_sm12].addr = addr_sm12;
+}
+}////////////////////////////////////////////////////////////////////////
 
-/*the ballot result function determines and implements voting result*/
-function BallotResult () returns (bool result) {
-if (block.number < Prop.EndingBlock) throw;
-if (Core.ActivePro == false) throw;
+/////////////////////////NEWTON WHITELIST////////////////////////////////
+function NewWhiteList(address _newtonpass){
+if(msg.sender == Apollo){
+if(MintOnePass[2].authorised == false)throw;   
+NewtonPass[_newtonpass].move = true;
+}
+if(msg.sender == Atlas){
+if(MintOnePass[4].authorised == false)throw;
+NewtonPass[_newtonpass].move = true;
+}
+}////////////////////////////////////////////////////////////////////////
 
-if (YesResult() == true && Prop.ChangeVoteRules == 1){
-UpdateVoteCalc();    
-}  
-if (YesResult() == true && Prop.AuthorityProposal == 1){
-/*set up call to sok mint one*/
-SokMintOne
-SokMintOneCall = SokMintOne(MinAuthVotePass[5].addr);
-(SokMintOneCall.ChangeAuth(ChangeA.Key,ChangeA.Auth));
-}  
-if (YesResult() == true){
-VoteResult.VotePassed = 'YES';    
-}  
-if (YesResult() == false){
-VoteResult.VotePassed = 'NO';    
-}  
-Core.ActivePro = false;
-Core.ActiveRulesPro = false; 
-Core.ActiveAuthPro = false;	
+///////////////////////SIRUS WHITELIST//////////////////////////////////	
+function SirWhiteList (address _siruspass){
+if(msg.sender == Apollo){
+if(MintOnePass[2].authorised == false)throw;	
+SirusPass[_siruspass].movement = true;	
+}	
+if(msg.sender == Atlas){
+if(MintOnePass[4].authorised == false)throw;	
+SirusPass[_siruspass].movement = true;		
+}		
+}////////////////////////////////////////////////////////////////////	
+		
+///////////////CHANGE KEY AUTHORITY STATUS BY VOTE//////////////////
+function ChangeAuth(uint8 _key, uint8 _auth) {
+/*security check*/
+if(MintOnePass[6].addr == msg.sender && _auth == 0){ 			
+MintOnePass[_key].authorised = false;
+}
+if(MintOnePass[6].addr == msg.sender && _auth == 1){ 			
+MintOnePass[_key].authorised = true;
+}		
+}///////////////////////////////////////////////////////
 
-/*log voting result on the blockchain*/
-VotingResultLog (ProposalID, Prop.ProposalTitle,Prop.DocUrl,VoteResult.NumVoters, 
-VoteResult.NumYesVotes, VoteResult.NumNoVotes,VoteResult.NumAbstainVotes, 
-VoteResult.AmountYesShares,VoteResult.TotalVoteShares,VoteResult.VotePassed);
-ProposalID++;
-return true;
-}//////////////////////////////end of ballot result function
+//////////////////////////////BALANCE FUNCTIONS//////////////////////
+/*External contracts can call to get coin balance of an address*/
+function FloBal(address _user) returns (uint256) { 
+uint256 flo_bal = balanceOf[_user].Florin; 
+return flo_bal;	
+}////////////////////////////////////////////////////////////////
+function NewBal(address _user) returns (uint256) {
+uint256 new_bal = balanceOf[_user].Newton;
+return new_bal;	
+}////////////////////////////////////////////////////////////////
+function SirBal(address _user) returns (uint256) {
+uint256 sir_bal = balanceOf[_user].Sirus;
+return sir_bal;	
+}////////////////////////////////////////////////////////////////
 
-/* function that calculates voting outcome*/
-function YesResult() internal returns (bool){
-uint256 Fraction = 100;	
+///////////FREEZE FLORIN BY VOTE & COMMITTEE///////////////////////////
 
-uint256 VotingQuorum = Prop.IssuedFlorin * (Core.ShareQuorum / Fraction);    
-uint256 YesVoteShares = (VoteResult.AmountYesShares * Fraction) / VoteResult.TotalVoteShares;
-uint256 YesVoteVoters = (VoteResult.NumYesVotes * Fraction) / VoteResult.NumVoters;
+/*Set florin freeze status*/
+function FreezeFlo(uint8 freeze_f) returns (bool sucess){
+/*security and rule checks*/
+if(MintOnePass[3].addr == msg.sender){
+if(MintOnePass[3].authorised == false) throw;
+FreezeFlorin = freeze_f;	
+} 
+if(MintOnePass[4].addr == msg.sender){
+if(MintOnePass[4].authorised == false) throw; 
+FreezeFlorin = freeze_f;
+}
+if(MintOnePass[7].addr == msg.sender){
+FreezeFlorin = freeze_f;
+} 	
+}/////////////////////////////////////////////////////////
 
-if ( VoteResult.TotalVoteShares >= VotingQuorum && YesVoteShares >= 
-Core.YesShares && YesVoteVoters >= Core.YesVoters) {
-return true; } else { return false; }
-}///////////////////////////////////////// end of yes result function 	
+/////////////////////////////////FLORIN FREEZE STATUS///////////////////////////
+/*Determine the freeze status of florin*/
+function FreezeInfo() returns (uint8) {
+return FreezeFlorin;
+}////////////////////////////////////////////////////////////////////////////////
 
-/*this function cleans all data stores except core*/
-function Clean() returns (bool){
-if(Core.ActivePro == true) throw;
-delete Prop;  
-delete ChangeVoteCalc;
-delete ChangeA;	
-delete VoteResult;	
-return true;
-}//////////////////////////////////////////end of clean function
+/////////////////////////////////FLORIN BUY BACK//////////////////////////
+function FloBuyBack(address _from, address _to, uint256 _value) returns (bool){
+if(FreezeFlorin == 1) throw; //check the florin freeze status
+if(MintOnePass[11].addr != msg.sender) throw; //check who is calling the function
+if(balanceOf[_from].Florin < _value) throw; //Check if the sender has enough value to send
+if(balanceOf[_to].Florin + _value < balanceOf[_to].Florin) throw; //Check for overflows
+balanceOf[_from].Florin -= _value;  //Subtract from the sender
+balanceOf[_to].Florin += _value;   //Add the same to the recipient
+Transfer(_from, _to, _value);     //broadcast transaction
+return true;	
+}////////////////////////////////////////////////////////////////////////////////////////
 
-/*this function updates the vote calculator data store*/
-function UpdateVoteCalc() internal returns (bool){
+//////////////////////////////////COIN TRANSACTIONS/////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 
-Core.ShareQuorum = ChangeVoteCalc.ChangeShareQuorum;
-Core.YesShares = ChangeVoteCalc.ChangeYesShares;
-Core.YesVoters = ChangeVoteCalc.ChangeYesVoters;
-return true;
-}////////////////////////////////////end of update vote calc function  
-}////////////////////////////////////end of mint_auth_vote_1 contract
+/*send florin coins*/
+function SendFlorin(address _to, uint256 _value) returns (bool){
+if(FreezeFlorin == 1) throw; //check the florin freeze status
+if(balanceOf[msg.sender].Florin < _value) throw; // Check if the sender has enough value to send
+if(balanceOf[_to].Florin + _value < balanceOf[_to].Florin) throw; //Check for overflows
+balanceOf[msg.sender].Florin -= _value;  //Subtract from the sender
+balanceOf[_to].Florin += _value;        //Add the same to the recipient
+Transfer(msg.sender, _to, _value);     //broadcast transaction
+return true;	
+}////////////////////////////////////////////////////////////////////////////////////////
+
+/*send newton coins*/
+function SendNewton(address _to, uint256 _value) returns (bool){
+if(NewtonPass[msg.sender].move != true) throw;	  //check who is calling the function
+if(balanceOf[msg.sender].Newton < _value) throw; //Check if the sender has enough value to send
+if(balanceOf[_to].Newton + _value < balanceOf[_to].Newton) throw; // Check for overflows
+balanceOf[msg.sender].Newton -= _value;  //Subtract from the sender
+balanceOf[_to].Newton += _value;        //Add the same to the recipient
+Transfer(msg.sender, _to, _value);     //broadcast transaction
+return true;	
+}////////////////////////////////////////////////////////////////////////////////////////
+
+/*send sirus coins*/
+function SendSirus(address _from, address _to, uint256 _value) returns (bool){
+if(SirusPass[msg.sender].movement != true) throw; //check who is calling the function	
+if(balanceOf[_from].Sirus < _value) throw; //Check if the sender has enough value to send
+if(balanceOf[_to].Sirus + _value < balanceOf[_to].Sirus) throw; // Check for overflows
+balanceOf[_from].Sirus -= _value;  //Subtract from the sender
+balanceOf[_to].Sirus += _value;   //Add the same to the recipient
+Transfer(_from, _to, _value);    //broadcast transaction
+return true;	
+}////////////////////////////////////////////////////////////////////////////////////////
+}/////////////////////////////////////////////////end of sok_mint_one contract
+/////////////////////////////////////////////////////////////////////////////////////////
+
